3fef2143d766b17b0b7dbb51306072bcfb145ef2
Vanessa Tavares Tavernari <vanessa.tavernari@gmail.com>
2022-03-28 00:06:08 +0100

    Initial commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vanessa T Tavernari on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
